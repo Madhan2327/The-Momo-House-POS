e7841e95e88a3d040d384a23838339121b970190
Madhan2327 <madhancb2306@gmail.com>
2026-07-15 17:08:37 +0530

    Fix API base URL

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -1,3 +1,6 @@
+import { Routes, Route } from "react-router-dom";
+import Layout from "../components/layout/Layout";
+
 import Login from "../pages/Login/Login";
 import Dashboard from "../pages/Dashboard/Dashboard";
 import Billing from "../pages/Billing/Billing";
